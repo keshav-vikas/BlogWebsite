e5c1ffbbc74630f67441e25acca76fa21cd25ec9
keshav-vikas <keshav@200997@gmail.com>
2021-11-30 16:17:00 +0530

    commit

---
+++ my_site/blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.db import models
+from django.forms import fields
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['post']
+        labels = {
+            'user_name': 'Your Name',
+            'user_email': 'Your Email',
+            'text': 'Your Comment'
+        }
